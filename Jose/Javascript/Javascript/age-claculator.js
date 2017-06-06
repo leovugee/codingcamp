e73391b68c7a226658ad11067681f5996a960665
Jose <josearmando.jacq@gmail.com>
2017-06-06 15:31:56 +0200

    jose files

---
+++ Jose/Javascript/Javascript/age-claculator.js
@@ -0,0 +1,7 @@
+var birthdayYear = 1992;
+var futureYear = 2999;
+var years = futureYear - birthdayYear;
+
+var calculator = "I will be either " + (years - 1) + " or " + years + " in " + futureYear;
+
+console.log(calculator);
