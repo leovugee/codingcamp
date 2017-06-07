b7310ac314c410c7a1723a4948cde5e7d9e4d047
Joe <donjoeberlinhood@googlemail.com>
2017-06-07 09:18:59 +0200

    javascript

---
+++ moejoe/javascript/supply.js
@@ -0,0 +1,8 @@
+var curAge = 31;
+var maxAge = 93;
+var iceCream = 10;
+var dayYear = 365;
+var reAge = maxAge - curAge;
+var totIcecream = reAge * (iceCream * dayYear);
+
+console.log("You will need " + totIcecream + " to last you until the ripe old age of " + maxAge);
