6c0c5f5e44f19f6130b072674524ccd140e21db8
rami <rami4luv1987@gmail.com>
2017-06-07 09:44:45 +0200

    javascript

---
+++ rami2/JavaScript/script/fortune.js
@@ -0,0 +1,8 @@
+var numKids  = 2;
+var partner  = "lena";
+var geoLocation = "syria";
+var jobTitle = "barber";
+
+var future = "You will be a " + jobTitle + " in " + geoLocation + ", and married to " +
+   partner + " with " + numKids + " kids.";
+console.log(future);
