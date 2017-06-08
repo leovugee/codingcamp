51b8017138ec87a8cd1b90eb7d19ae0b73931956
mir <mirnumbing@gmail.com>
2017-06-08 09:21:54 +0200

    first java

---
+++ mir/javascripts/scripts/circle.js
@@ -0,0 +1,6 @@
+var r = 3;
+var circumference = Math.round (2 * r * Math.PI);
+var area = Math.round ( Math.pow(r, 2) * Math.PI);
+
+document.write("The circumference is " + circumference + "<br>");
+document.write("The area is " + area);
