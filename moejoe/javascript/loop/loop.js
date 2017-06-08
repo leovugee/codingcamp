ca9d5cf85fdabdec5afd68463653f46cd5f4737d
Joe <donjoeberlinhood@googlemail.com>
2017-06-08 16:55:45 +0200

    java

---
+++ moejoe/javascript/loop/loop.js
@@ -0,0 +1,11 @@
+
+function loop() {
+	var multi = document.getElementById("click").value;
+	var message = "";
+
+	for (var counter = 0; counter <= 10; counter++){
+		re = multi * counter  ;
+		message = message + counter + " * " + multi + " = " + re + "<br>";
+}
+	document.getElementById("result").innerHTML = message;
+}
