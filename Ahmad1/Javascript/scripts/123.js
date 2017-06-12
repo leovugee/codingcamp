aac25048d54acc5192d9fff466e7243f3f649c24
Ahmad othman <othman0ahmad@gmail.com>
2017-06-12 10:01:00 +0200

    new

---
+++ Ahmad1/Javascript/scripts/123.js
@@ -0,0 +1,11 @@
+var  multiplier =10;
+var total = 10;
+
+for(multiplier = 1; multiplier <= total; multiplier++) {
+
+	for(var i = 0; i <= total; i++) {
+		var result  = multiplier * i;
+		console.log(+ i + " * " + multiplier + " = " + result);
+	}
+
+}
