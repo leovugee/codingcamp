6d4a366f90a0e91e1df76f7884ebbe837c88aef8
Jose <josearmando.jacq@gmail.com>
2017-06-14 23:31:27 +0200

    New files

--- Jose/Javascript/Javascript/pluralize.js
+++ Jose/Javascript/Javascript/pluralize.js
@@ -1,42 +1,11 @@
-function greaterNum(){
-    var animal =  prompt("Which animal do you you want?");
-    var num = prompt("How many animals do you want");
-    var sheep = ["s", "h","e", "e","p"];
-    var temp = true;
-    
-    if(animal == null || animal == ""){
-        window.alert("You didnt wrote a animal", "");
-        
-    }
-    if(num == null || num == 0){
-        window.alert("You didnt wrote a num", "");
-        
-    }
-    
-    animal = animal.split("");
-    
-    if(num > 1){
-        
-        for(var i = 0; i < animal.length; i++){
-            
-            
-            if(animal[i] != sheep[i]){
-                temp = false;
-                
-            }
-        }
-               
-            if(temp == true){
-                animal = animal.join("");
-                document.getElementById('text').innerHTML = "You have " + num + " " + animal;    
-            }
-        else{
-            animal.push("s");
-            animal = animal.join("");
-            document.getElementById('text').innerHTML = "You have " + num + " " + animal;    
-        }
+function greaterNum(num, name){
+   if(num > 1 && name != "" & name != "sheep"){
+        document.getElementById("text").innerHTML = num + " " + name + 's';
+      }
+    if(num < 1 || name === "sheep"){
+        document.getElementById("text").innerHTML = num + " " + name;
     }
     
 }
 
-greaterNum();
+greaterNum(2, "dog");
