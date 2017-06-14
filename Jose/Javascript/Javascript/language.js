5f71c867db52c3830946230bcebb302d60ba75bc
Jose <josearmando.jacq@gmail.com>
2017-06-14 21:32:33 +0200

    New files

---
+++ Jose/Javascript/Javascript/language.js
@@ -0,0 +1,12 @@
+function showMessage(val){
+    if( val == "italian"){
+        alert("Ciao");
+    }else if(val == "spanish"){
+        alert("Hola");
+    }else if(val == "german"){
+        alert("Hallo");
+    }else{
+        alert("bonjour");
+    }
+}
+
