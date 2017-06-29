f3fa13e02eb0e773663ee051a0de349d411729b2
Jose <josearmando.jacq@gmail.com>
2017-06-29 10:05:19 +0200

    news updates

--- Jose/Javascript/Javascript/calculator.js
+++ Jose/Javascript/Javascript/calculator.js
@@ -1,72 +1,52 @@
 var result1;
 var aar = [];
 aar = document.getElementsByTagName("button");
+var value1 = document.getElementById("num1").value;
+var value2 = document.getElementById("num2").value;
 
 function rest() {
     document.getElementById("num1").value = 0;
     document.getElementById("num2").value = 0;
     calculator("sum");
 }
-function reset(){
-    document.getElementById("num1").value = "";
-    document.getElementById("num2").value = "";
-}
-
-function calculator(val){
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    switch(val){
-    
-        case 'sum':
-            
-                
-                
-                 result1 = parseInt(num1) + parseInt(num2); 
-                
-            
-            for(var i = 0; i < aar.length; i++){
+function changeColor(){
+    for(var i = 0; i < aar.length; i++){
                 aar[i].style.color = "black";
             }
             document.getElementById("sum").style.color = "red";
+}
+
+function calculator(val){
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    switch(val){
+    
+        case 'sum':
+            result1 = num1 + num2; 
+            changeColor();
             break;
             
         case 'mul':
-            
-            
-            result1 = parseInt(num1) * parseInt(num2); 
-            
-            for(var i = 0; i < aar.length; i++){
-                aar[i].style.color = "black";
-            }
-            document.getElementById("mul").style.color = "red";
+            result1 = num1 * num2; 
+            changeColor();
             break;
             
         case 'min':
             
-            result1 = parseInt(num1) - parseInt(num2);   
-            for(var i = 0; i < aar.length; i++){
-                aar[i].style.color = "black";
-            }
-            document.getElementById("min").style.color = "red";
+            result1 = num1 - num2;   
+            changeColor();
             break;
             
         case 'div':
              if(num2 != 0){
-              result1 = parseInt(num1) / parseInt(num2);
-                for(var i = 0; i < aar.length; i++){
-                aar[i].style.color = "black";
-                }
-                document.getElementById("div").style.color = "red";
-                
+              result1 = num1 / num2;
+                changeColor();
                 break;
             }
     
             else{
                 alert("Its not possible to divide");
-                for(var i = 0; i < aar.length; i++){
-                aar[i].style.color = "black";
-                }
-                document.getElementById("div").style.color = "red";
+               changeColor();
                 break;
             }
                
@@ -77,10 +57,10 @@ function calculator(val){
 
 function result(){
  
-    if( document.getElementById("num1").value == "" || document.getElementById("num2").value == ""){
+    if( value1 == "" || value2 == ""){
                document.getElementById('text').innerHTML =  "You have to write the two numbers first";
                 
-     }else if( isNaN(document.getElementById("num1").value)  || isNaN(document.getElementById("num2").value)){
+     }else if( isNaN(value1)  || isNaN(value2)){
          document.getElementById('text').innerHTML =  "Put a number motherfucker ;) ";
      }else{
     document.getElementById('text').innerHTML = result1;
