50a1e57c34beb981d2bbe13a48d244dc29496ada
Jose <josearmando.jacq@gmail.com>
2017-07-10 16:12:34 +0200

    adding more stuffs

---
+++ Jose/JQuery/moving button without animate/moving-button.js
@@ -0,0 +1,15 @@
+var button = $("#button");
+
+
+$(document).ready(function(){
+  button.on("click", function(){
+    if(button.text() === "X"){
+    button.addClass("animate");
+    button.text("Y");
+  }else{
+    button.addClass("animate1");
+    button.text("X");
+  }
+
+  });
+});
